97b87228c2da6abbe47e8159243990371905c742
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-29 14:25:42 -0800

    Update length-of-missing-array.js

--- length-of-missing-array.js
+++ length-of-missing-array.js
@@ -42,3 +42,17 @@ function getLengthOfMissingArray(arrayOfArrays) {
 }
 
 // or
+
+function getLengthOfMissingArray(arrayOfArrays) {
+  let result = (arrayOfArrays || []).map(e=> e ? e.length : 0)
+        .sort((a,b) => a - b)
+    if(result.includes(0)){
+        return 0;
+    }
+    for(let i = 0; i < result.length; i++){
+        if(result[i+1] != result[i]+1){
+            return result[i]+1;
+        }
+    }
+    return 0
+}
