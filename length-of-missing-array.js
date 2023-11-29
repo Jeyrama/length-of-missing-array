b68c1f7e6d07c534ac970d7b746cc3736795c3a4
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-29 14:23:00 -0800

    Update length-of-missing-array.js

--- length-of-missing-array.js
+++ length-of-missing-array.js
@@ -22,3 +22,20 @@ its length will be always between the given arrays.
 
 
 // Solution
+
+function getLengthOfMissingArray(arrayOfArrays) {
+  const lengths = (arrayOfArrays || [])
+    .map(a => a ? a.length : 0)
+    .sort((a, b) => a - b)
+
+  if (lengths.includes(0)) {
+    return 0
+  }
+
+  for (let i = 0; i < lengths.length -1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1
+    }
+  }
+  return 0
+}
